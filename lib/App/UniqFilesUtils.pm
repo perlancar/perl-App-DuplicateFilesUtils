4d02d0c36605a00bd141c738d5f2486f431c9fc2
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2022-11-13 18:07:47 +0700

    Add pointer synonym module: App::UniqFilesUtils

---
+++ lib/App/UniqFilesUtils.pm
@@ -0,0 +1,17 @@
+package App::UniqFilesUtils;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+#ABSTRACT: (See App::DuplicateFilesUtils)
+
+=head1 SEE ALSO
+
+L<App::DuplicateFilesUtils>
+
+=cut
